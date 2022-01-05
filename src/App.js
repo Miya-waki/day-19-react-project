a4463c659dc64768abf5888894f6c904cebf9dd1
Miya-waki <rahulgiri12@gmail.com>
2022-01-05 07:55:33 +0530

    b5

--- src/App.js
+++ src/App.js
@@ -1,52 +1,50 @@
 import { useState } from "react";
-export default function App(){
-  return (
-    <div>
 
-      <MyComoponent/>
-    </div>
+export default function App() {
+  return (
+    <>
+      <MyComponent />
+    </>
   );
 }
 
+function MyComponent() {
+  let [counter, setCounter] = useState(0);
 
-function MyComoponent(){
-  const id1 =100;
-  const background ="bg-info";
-  
-   
-    const list = [1, 1, 1, 1, 1, 1, 1, 1, 1];
-    const [cardCss, setCardcss] = useState("bg-primary text-light fs-3 p-4 mb-1");
-  
-    const primaryTheme = () => setCardcss("bg-primary text-light fs-3 p-4 mb-1");
-    const darkTheme = () => setCardcss("bg-dark text-light fs-3 p-4 mb-1");
-    const dangerTheme = () => setCardcss("bg-danger text-light fs-3 p-4 mb-1");
-  
-    return (
+  const increment = () => {
+    counter = counter + 1;
+
+    // RERENDER THE UI :: UPDATE THE INNERHTML ; innerHTML
+    setCounter(counter);
+  };
+
+  return (
+    <div>
+      <h1>Counter Application</h1>
+      <input type="button" value="Click Me" onClick={increment} />
       <div>
-        <input
-          type="button"
-          value="Primary Theme"
-          className="btn btn-primary btn-sm"
-          onClick={primaryTheme}
-        />
-        <input
-          type="button"
-          value="Primary Theme"
-          className="btn btn-dark btn-sm"
-          onClick={darkTheme}
-        />
-        <input
-          type="button"
-          value="Danger Theme"
-          className="btn btn-danger btn-sm"
-          onClick={dangerTheme}
-        />
-        {list.map(() => (
-          <div className={cardCss}>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur,
-            earum. {cardCss}
-          </div>
-        ))}
+        <h1>Like {counter}</h1>
       </div>
+      <div>Like {counter} </div>
+      <div>Like {counter} </div>
+      <div>Like {counter} </div>
+      <div>Like {counter} </div>
+      <div>Like {counter} </div>
+      <div>Like {counter} </div>
+      <div>Like {counter} </div>
+      <div>Like {counter} </div>
+      <div>Like {counter} </div>
+      <div>Like {counter} </div>
+      <div>Like {counter} </div>
+      <div>Like {counter} </div>
+      <div>Like {counter} </div>
+      <div>Like {counter} </div>
+      <div>Like {counter} </div>
+      <div>Like {counter} </div>
+      <div>Like {counter} </div>
+      <div>Like {counter} </div>
+      <div>Like {counter} </div>
+      <div>Like {counter} </div>
+    </div>
   );
 }
