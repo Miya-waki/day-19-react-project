76c9362c380653186dd0296c4c44fc27d3f0b74d
Miya-waki <rahulgiri12@gmail.com>
2022-01-05 08:15:13 +0530

    b10

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
+import { useState } from "react";
+
 export default function App() {
   return (
-    <div className="bg-danger">
+    <>
       <MyComponent />
-    </div>
+    </>
   );
 }
 
 function MyComponent() {
+  // 1
+  const [message, setMessage] = useState("");
+  const [messageList, setMessageList] = useState([]);
+
+  // 3
+  const processMessage = (e) => {
+    const newMessage = e.target.value;
+    setMessage(newMessage);
+  };
+
+  // 4.
+  const addTweet = () => {
+    console.log(message);
+    const newMessageList = [message, ...messageList];
+    setMessageList(newMessageList);
+    setMessage("");
+  };
+
   return (
     <div>
-      <h1>Learning interpolation</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto ex
-        repudiandae optio alias, autem doloremque sunt tenetur deleniti, maxime
-        explicabo dolorem laborum? Sapiente amet deleniti voluptatibus magnam!
-        Tempore, doloremque. Corporis?
-      </p>
+      <h1>Input Operation</h1>
+      <input
+        type="text"
+        placeholder="Whats App...."
+        value={message}
+        onChange={processMessage}
+      />
+      <input type="button" value="Tweet" onClick={addTweet} />
+
+      {messageList.map((item, index) => (
+        <div key={index}> {item} </div>
+      ))}
     </div>
   );
 }
